ee5690f03fefa581bb9a129545ed631dbd805b12
rmcaut <bobcautserv@gmail.com>
2025-11-12 19:42:54 -0300

    added itertool usage for combinations

--- auxiliar.py
+++ auxiliar.py
@@ -3,16 +3,15 @@
 # for c in alph:
 #     print(c.lower(), end='')
 
-# Rangoli
-def print_rangoli(size):
-    alph = list(map(chr, range(97, 123)))
-    alph_lower = [c.lower() for c in alph]
-    for rows_upper in range(1, size - 1): 
-        print('-'*2*(size - rows_upper), end='')
-        for l in range(1, rows_upper):
-            print(alph_lower[size - l]+'-', end='')
-        
-     
+
+# open a file to write results from function
+import os
 if __name__ == '__main__':
-    n = int(input())
-    print_rangoli(n)
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = myfunction(s)
+    fptr.write(result + '\n')
+    fptr.close()
+
+
+#
